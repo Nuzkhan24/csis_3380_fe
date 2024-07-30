0e6ef8547049b8c98a78efbd8f2c8c75b388d3ac
Tamoor Haider <tamoor.h.aslam@gmail.com>
2024-07-29 20:41:12 -0700

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import MenuBar from './components/MenuBar.js';
 import Dashboard from './components/Dashboard.js';
 
